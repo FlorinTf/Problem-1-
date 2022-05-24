b480b984802d4855756855ed1f7df6960933046d
Florin Tirlogeanu <96166795+FlorinTf@users.noreply.github.com>
2022-05-24 12:11:52 +0300

    Update Windows-Task-Manager-App.py

--- Windows-Task-Manager-App.py
+++ Windows-Task-Manager-App.py
@@ -1,13 +1,3 @@
-'''
-Problem 1
-Implement a program that will launcha specified process and periodically (with aprovided time interval) collect the following data about it:
-•	CPU usage (percent);
-•	Memory consumption: Working Set and Private Bytes (for Windows systems)or Resident Set Size and Virtual Memory Size (for Linux systems);
-•	Number of open handles (for Windows systems)or file descriptors (for Linux systems).
-Data collection should be performed all the time the process is running. Path to the executable file for the process and time interval between data
- collection iterations should be provided by user. Collected data should best stored on the disk. Format of stored data should support automated
-parsing to potentially allow, for example, drawing of charts.
-'''
 import time
 import psutil
 import csv
